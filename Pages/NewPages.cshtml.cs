14ed8ba32aaac927918080558fd1a69cb4e477b7
Miguel Germano <miguelgermano86@gmail.com>
2023-03-31 17:28:29 -0300

    Up_Razor_Pages

---
+++ Pages/NewPages.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages
+{
+    public class NewPagesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
